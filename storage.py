858859992fb77a441e9ab6bc134771afe82d2c1d
Anirudh Koduganti <anikodu@mac.lan>
2026-07-28 16:56:26 -0400

    fix: remove double API call, standardize output paths

--- storage.py
+++ storage.py
@@ -1,9 +1,12 @@
 import json
+import os
 
 def save_report(ticker, data):
-    with open (f"{ticker}.json", "w", encoding="utf-8") as file:
+    if not os.path.exists("reports"):
+        os.makedirs("reports")
+    with open (f"reports/{ticker}.json", "w", encoding="utf-8") as file:
         return json.dump(data, file, indent=4)
 
 def load_report(ticker):
-    with open (f"{ticker}.json", "r", encoding="utf-8") as file:
+    with open (f"reports/{ticker}.json", "r", encoding="utf-8") as file:
         return json.load(file)
