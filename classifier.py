17c55d53059336c1e0c2cbc390f0631f2386b3a0
Anirudh Koduganti <anikodu@mac.lan>
2026-07-30 18:33:50 -0400

    feat: TF-IDF vectorizer + Naive Bayes classifier

--- classifier.py
+++ classifier.py
@@ -1,6 +1,14 @@
 from storage import load_report 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 companies = ["AAPL", "MSFT", "JPM", "NVDA"]
+
+text_train = []
+label_train = []
+
+test_text = []
+test_label = []
 
 data = []
 
@@ -15,13 +23,36 @@ for company in companies:
                 "label": section_name
             }
         )
-count = {}
+label_count = {}
 
 for item in data:
     label = item["label"]
-    if label in count:
-        count[label] += 1
+    if label in label_count:
+        label_count[label] += 1
     else: 
-        count[label] = 1
+        label_count[label] = 1
 
-print(count)
+print(label_count)
+
+for item in data:
+    if item["company"] == "NVDA":
+        test_text.append(item["text"])
+        test_label.append(item["label"])
+    else:
+        text_train.append(item["text"])
+        label_train.append(item["label"])
+
+vectorizer = TfidfVectorizer()
+train_vectors = vectorizer.fit_transform(text_train)
+test_vectors = vectorizer.transform(test_text)
+
+classifier = MultinomialNB()
+classifier.fit(train_vectors, label_train)
+
+predictions = classifier.predict(test_vectors)
+
+index = 0
+
+for label in test_label:
+    print("Actual: ", label, "Predicted: ", predictions[index])
+    index+=1
