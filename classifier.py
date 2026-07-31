71c267cbdea0e0ed61ba31466fde212e77c515e8
Anirudh Koduganti <anikodu@Anirudhs-Laptop.local>
2026-07-30 23:14:43 -0400

    eval: compare TF-IDF classifier vs rule-based tagger

--- classifier.py
+++ classifier.py
@@ -1,6 +1,9 @@
 from storage import load_report 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from section_tagger import tag_section
+
 
 companies = ["AAPL", "MSFT", "JPM", "NVDA"]
 
@@ -51,8 +54,34 @@ classifier.fit(train_vectors, label_train)
 
 predictions = classifier.predict(test_vectors)
 
+rule_predic = []
+
+for text in test_text: 
+    rule_predic.append(tag_section(text))
+
+print(classification_report(test_label, predictions))
+
 index = 0
 
 for label in test_label:
-    print("Actual: ", label, "Predicted: ", predictions[index])
+    print(
+        "Actual: ", label,
+        "TF-IDF Prediction: ", predictions[index],
+        "Rule-Based Prediction: ", rule_predic[index]
+    )
     index+=1
+
+"""
+Results: 
+
+The TF-IDF + Naive Bayes classifier correctly identified three out of the four sections for NVDA. It identified the Business, Risk Factors, 
+and Financial Statements sections correctly, but incorrectly classified the MD&A section as the Business section. 
+
+The Rule-Based tagger correctly classified the Business and MD&A sections, but misclassified the Risk Factors section as Business. It also
+labeled the Financial Statements section as unknown because it was not designed to classify that section. 
+
+Overall, the TF-IDF + Naive Bayes classifier performed better than the Rule-Based tagger on the NVDA test set. However, the small training dataset
+of only three companies made it harder for the model to distinguish between Business and MD&A, since those sections can often contain similar
+vocabulary
+
+"""
